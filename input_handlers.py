fa3a385a32c079a6c8e03a224ebd32409104b517
David Sturgis <david@smarterer.com>
2019-10-08 11:45:36 -0400

    Add numeric keypad support

--- input_handlers.py
+++ input_handlers.py
@@ -41,21 +41,22 @@ def handle_player_turn_keys(key):
     # Movement keys
     key_char = chr(key.c)
 
-    if key.vk == libtcod.KEY_UP or key_char == 'k':
+    # TODO refactor me into mappings of keysets and actions!
+    if key.vk in (libtcod.KEY_UP, libtcod.KEY_KP8) or key_char == 'k':
         return {'move': (0, -1)}
-    elif key.vk == libtcod.KEY_DOWN or key_char == 'j':
+    elif key.vk in (libtcod.KEY_DOWN, libtcod.KEY_KP2) or key_char == 'j':
         return {'move': (0, 1)}
-    elif key.vk == libtcod.KEY_LEFT or key_char == 'h':
+    elif key.vk in (libtcod.KEY_LEFT, libtcod.KEY_KP4) or key_char == 'h':
         return {'move': (-1, 0)}
-    elif key.vk == libtcod.KEY_RIGHT or key_char == 'l':
+    elif key.vk in (libtcod.KEY_RIGHT, libtcod.KEY_KP6) or key_char == 'l':
         return {'move': (1, 0)}
-    elif key_char == 'y':
+    elif key.vk == libtcod.KEY_KP7 or key_char == 'y':
         return {'move': (-1, -1)}
-    elif key_char == 'u':
+    elif key.vk == libtcod.KEY_KP9 or key_char == 'u':
         return {'move': (1, -1)}
-    elif key_char == 'b':
+    elif key.vk == libtcod.KEY_KP1 or key_char == 'b':
         return {'move': (-1, 1)}
-    elif key_char == 'n':
+    elif key.vk == libtcod.KEY_KP3 or key_char == 'n':
         return {'move': (1, 1)}
     elif key_char == 'z' or key_char == ' ':
         return {'wait': True}
